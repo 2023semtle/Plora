e28c804a8ae64647c5c0a11871a432eac697b840
bluefirewolf534 <bluefirewolf534@gmail.com>
2023-06-13 20:59:44 +0900

    :sparkles: Plan 클래스

--- src/main/java/team/banana/plan/Plan.java
+++ src/main/java/team/banana/plan/Plan.java
@@ -5,12 +5,32 @@ public class Plan {
     private int id;
     private String content;
 
+    private boolean isComplete = false;
+
+    public Plan(int id, String content, boolean isComplete){
+        this.id = id;
+        this.content = content;
+        this.isComplete = isComplete;
+    }
+
     public Plan(int id, String content){
         this.id = id;
         this.content = content;
     }
 
+    public int getId() {
+        return id;
+    }
+
     public String getContent() {
         return content;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete) {
+        isComplete = complete;
     }
 }
