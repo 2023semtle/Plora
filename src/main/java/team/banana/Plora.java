6bcdfcf0ea47c578d217b88a7449efacb2afbe45
jjwon419 <bluefirewolf534@gmail.com>
2023-06-16 21:45:56 +0900

    :sparkles: MainWindow 추가

--- src/main/java/team/banana/Plora.java
+++ src/main/java/team/banana/Plora.java
@@ -2,9 +2,14 @@ package team.banana;
 
 import team.banana.plan.Plan;
 import team.banana.plan.PlanFactory;
+import team.banana.windows.MainWindow;
 
 public class Plora {
+
+    private static MainWindow mainWindow;
     public static void main(String[] args){
         PlanFactory.init();
+        mainWindow = new MainWindow();
+        mainWindow.setVisible(true);
     }
 }
