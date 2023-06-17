c9f3e3b3b05f906f3db5aa641ed29ccfaf504a93
bluefirewolf534 <bluefirewolf534@gmail.com>
2023-06-17 09:54:20 +0900

    :art: MainWindow 창 크기 조절

--- src/main/java/team/banana/windows/MainWindow.java
+++ src/main/java/team/banana/windows/MainWindow.java
@@ -5,6 +5,6 @@ import javax.swing.*;
 public class MainWindow extends JFrame {
     public MainWindow(){
         setTitle("Plora");
-        setSize(1500, 800);
+        setSize(1200, 600);
     }
 }
